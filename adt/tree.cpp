e9f8bc82e85d0693a88a71167fd29b11d3cfdf3a
booirror <booirror@gmail.com>
2016-01-07 21:15:36 +0800

    t

--- adt/tree.cpp
+++ adt/tree.cpp
@@ -1,16 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class C {
+public:
+	int v;
+	C():v(0){}
+	C(int val) { v = val; }
+	~C() {
+		std::cout << "C::~C(), v=" << v << std::endl;
+	}
+
+};
+
 struct  PathNode
 {
-	Vec2Ex vex;
+	C c;
 	PathNode* child;
 	PathNode* sibling;
 
-	PathNode(const Vec2Ex& v) : vex(v), child(nullptr), sibling(nullptr)
+	PathNode(const C& v) : c(v), child(nullptr), sibling(nullptr)
 	{
 
 	}
 };
 
-void DropPath::deleteTree(Tree t)
+typedef PathNode *Tree;
+
+void deleteTree(Tree t)
 {
 	if (t == nullptr)
 		return;
@@ -25,4 +42,30 @@ void DropPath::deleteTree(Tree t)
 		sib = sib->sibling;
 		deleteTree(tmp);
 	}
+}
+
+void mainTree()
+{
+	Tree t = new PathNode(C(2));
+
+	PathNode* t2 = new PathNode(C(3));
+	t->child = t2;
+
+	PathNode* t3 = new PathNode(C(4));
+
+	t2->sibling = t3;
+
+	PathNode* t4 = new PathNode(C(5));
+	t3->sibling = t4;
+
+	PathNode* t5 = new PathNode(C(6));
+	t2->child = t5;
+
+	PathNode* t6 = new PathNode(C(7));
+	t5->child = t6;
+
+	PathNode* t7 = new PathNode(C(8));
+	t4->child = t7;
+
+	deleteTree(t);
 }
